e4de294fe17764ed220e47764fe9008ca489a259
Hanwook Ryu <ryu@hanwook.me>
2022-11-05 09:42:43 +0900

    Feat: when을 사용해보자

--- src/basics/EnumWithWhen.kt
+++ src/basics/EnumWithWhen.kt
@@ -1,10 +1,35 @@
 package elements
 
-enum class EnumWithWhen(val red: Int, val green: Int, val blue: Int) {
+class EnumWithWhen {}
+
+enum class Color(val red: Int, val green: Int, val blue: Int) {
     RED(255, 0, 0), ORANGE(255, 165, 0),
     YELLOW(255, 255, 0), GREEN(0, 255, 0),
     BLUE(0, 0, 255), INDIGO(75, 0, 130),
     VIOLET(238, 130, 238); // 여기 사용하는 세미콜론은 필수로 사용해야 한다.
 
     fun rgb() = (red * 256 + green) * 256 + blue
+}
+
+fun getMnemonic(color: Color) =
+    when (color) {
+        Color.RED -> "Richard"
+        Color.ORANGE -> "Of"
+        Color.YELLOW -> "York"
+        Color.GREEN -> "Gave"
+        Color.BLUE -> "Battle"
+        Color.INDIGO -> "In"
+        Color.VIOLET -> "Vain"
+    }
+
+fun getWarmth(color: Color) =
+    when (color) {
+        Color.RED, Color.ORANGE, Color.YELLOW -> "Warm"
+        Color.GREEN -> "Neutral"
+        Color.BLUE, Color.INDIGO, Color.VIOLET -> "Cold"
+    }
+
+fun main() {
+    println(getMnemonic(Color.RED))
+    println(getWarmth(Color.ORANGE))
 }
