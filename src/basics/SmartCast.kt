568dfdda1a25a4bbbb55ef80e960f3a8b4b7e030
Hanwook Ryu <ryu@hanwook.me>
2022-11-05 10:26:28 +0900

    Feat: 분기에서 블록을 사용해보기

--- src/basics/SmartCast.kt
+++ src/basics/SmartCast.kt
@@ -26,8 +26,25 @@ fun evalForWhen(e: Expr): Int =
         else -> throw IllegalArgumentException("Unknown expression")
     }
 
+fun evalWithLogging(e: Expr): Int =
+    when (e) {
+        is Num -> {
+            println("num ${e.value}")
+            e.value
+        }
+        is Sum -> {
+            val left = evalWithLogging(e.left)
+            val right = evalWithLogging(e.right)
+
+            println("sum $left + $right")
+            left + right
+        }
+        else -> throw IllegalArgumentException("Unknown expression")
+    }
+
 
 fun main() {
     println(eval(Sum(Sum(Num(1), Num(2)), Num(4))))
     println(evalForWhen(Sum(Sum(Num(1), Num(2)), Num(4))))
+    println(evalWithLogging(Sum(Sum(Num(1), Num(2)), Num(4))))
 }
