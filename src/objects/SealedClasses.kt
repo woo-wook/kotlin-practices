2db5dfa71cbcb94f8c82f0e92f5b98fc8f27bd0e
Hanwook Ryu <ryu@hanwook.me>
2022-11-13 12:39:21 +0900

    Feat: 봉인된 클래스

---
+++ src/objects/SealedClasses.kt
@@ -0,0 +1,13 @@
+package objects
+
+sealed class Expr {
+    class Num(val value: Int): Expr()
+    class Sum(val left: Expr, val right: Expr): Expr()
+}
+
+fun eval(e: Expr): Int =
+    when (e) {
+        is Expr.Num -> e.value
+        is Expr.Sum -> eval(e.left) + eval(e.right)
+    }
+
