eede039618b05c5c92a7c8e83222d500c9555a8d
Hanwook Ryu <ryu@hanwook.me>
2022-11-28 15:39:29 +0900

    Feat: 읽기 전용과 변경 가능한 컬렉션

---
+++ src/typesystem/MutableCollections.kt
@@ -0,0 +1,16 @@
+package typesystem
+
+fun <T> copyElements(source: Collection<T>, target: MutableCollection<T>) {
+    for(item in source) {
+        target.add(item)
+    }
+}
+
+fun main() {
+    val source: Collection<Int> = arrayListOf(3, 5, 7)
+    val target: MutableCollection<Int> = arrayListOf(1)
+
+    copyElements(source, target)
+
+    println(target)
+}
